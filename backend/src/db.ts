ef6006e5a6975edf426b2deec715de1f6494652a
Ashwin-Thulasiraman <ashwintdav@gmail.com>
2025-12-04 20:29:04 +0530

    Added db.ts

---
+++ backend/src/db.ts
@@ -0,0 +1,10 @@
+import pg from 'pg';
+const pool =new pg.Pool({
+
+    user:process.env.user,
+    host:'localhost',
+    database:process.env.database,
+    password:process.env.password,
+    port:5432
+});
+module.exports=pool;
